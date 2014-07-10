89bd7736314ae2163492a271eee1c9621eed9b9d
Alex Fish <alex@alexefish.com>
2014-07-10 20:44:16 +0100

    Updating command to remove dependency

--- lib/tug/commands/command.rb
+++ lib/tug/commands/command.rb
@@ -1,12 +1,6 @@
 class Command
 
-  def execute(*args)
-    parser = OptionParser.new(args)
-    parser.parse
-  end
-
-  def build
-    xcode = Xcode.new
-    xcode.build
+  def execute(options)
+    puts options
   end
 end
