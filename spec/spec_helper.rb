96c4ac2928e4523778ead2b59fcfff6b03d2abee
Alex Fish <alex@alexefish.com>
2014-07-11 20:55:39 +0100

    Sending test output to rspec file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,8 @@
 require "tug"
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    $stderr = File.new(File.join(File.dirname(__FILE__), '../tmp', 'rspec.txt'), 'w')
+    $stdout = File.new(File.join(File.dirname(__FILE__), '../tmp', 'rspec.txt'), 'w')
+  end
+end
