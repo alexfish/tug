f103634f2722a4589957d3a178ccebc707a3aa3b
Alex Fish <alex@alexefish.com>
2014-07-11 21:21:53 +0100

    Adding test out file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require "tug"
 
 RSpec.configure do |config|
   config.before(:suite) do
-    $stderr = File.new(File.join(File.dirname(__FILE__), '../tmp', 'rspec.txt'), 'w')
-    $stdout = File.new(File.join(File.dirname(__FILE__), '../tmp', 'rspec.txt'), 'w')
+    $stderr = File.new(File.join(File.dirname(__FILE__), 'output.txt'), 'w')
+    $stdout = File.new(File.join(File.dirname(__FILE__), 'output.txt'), 'w')
   end
 end
