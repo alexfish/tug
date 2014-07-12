2c2afff5136af33f52a57047f56dce13029d96e0
Alex Fish <alex@alexefish.com>
2014-07-12 13:36:39 +0100

    Executing xctool on build

--- lib/tug/command/build_command.rb
+++ lib/tug/command/build_command.rb
@@ -2,6 +2,10 @@ class BuildCommand < Command
 
   def execute(options)
     config_file = ConfigFile.config_file(options[:config])
-    puts "build: #{config_file.config}"
+    build(config_file.config)
+  end
+
+  def build(config)
+    exec("xctool -workspace #{config['workspace']} -scheme #{config['scheme']}")
   end
 end
--- spec/build_command_spec.rb
+++ spec/build_command_spec.rb
@@ -2,4 +2,25 @@ require 'spec_helper'
 
 describe BuildCommand do
 
+  describe "when executing" do
+
+    before(:each) do
+      @buld_command = BuildCommand.new
+    end
+
+    it "should run xctool" do
+      expect(@buld_command).to receive(:exec).with(/xctool/)
+      @buld_command.build({})
+    end
+
+    it "should set the workspace" do
+      expect(@buld_command).to receive(:exec).with(/-workspace test/)
+      @buld_command.build({"workspace" => "test"})
+    end
+
+    it "should set the scheme" do
+      expect(@buld_command).to receive(:exec).with(/-scheme test/)
+      @buld_command.build({"scheme" => "test"})
+    end
+  end
 end
