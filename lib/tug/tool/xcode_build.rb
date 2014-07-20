385789991998cc5241ebd6625c5e74a2aa06aeb7
Alex Fish <alex@alexefish.com>
2014-07-20 23:40:05 +0100

    Breaking down ipa export options

--- lib/tug/tool/xcode_build.rb
+++ lib/tug/tool/xcode_build.rb
@@ -2,7 +2,21 @@ module Tug
   class XcodeBuild
 
     def export_ipa(name, profile)
-      system("xcodebuild -archivePath /tmp/#{name}.xcarchive -exportPath /tmp/#{name}.ipa -exportFormat ipa -exportArchive -exportProvisioningProfile #{profile}")
+      system("xcodebuild #{archive_options(name)} #{export_options(name)} #{profile_options(profile)}")
+    end
+
+    private
+
+    def profile_options(profile)
+      "-exportProvisioningProfile #{profile}"
+    end
+
+    def export_options(name)
+      "-exportPath /tmp/#{name}.ipa -exportFormat ipa -exportArchive"
+    end
+
+    def archive_options(name)
+      "-archivePath /tmp/#{name}.xcarchive"
     end
   end
 end
