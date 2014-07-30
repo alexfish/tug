3b6a56695a5662427fb65f5db9137040772faf63
Alex Fish <fish@ustwo.co.uk>
2014-07-30 14:33:40 +0100

    Adding option aliases

--- lib/tug/interface/interface.rb
+++ lib/tug/interface/interface.rb
@@ -2,15 +2,15 @@ module Tug
   class Interface < Thor
 
     desc "build", "build a project"
-    option :config, :default => "#{Dir.pwd}/.tug.yml"
+    option :config, :default => "#{Dir.pwd}/.tug.yml", :aliases => "-c"
     def build
       config_file = Tug::ConfigFile.config_file(options[:config])
       execute(__method__.to_s, config_file)
     end
 
     desc "ipa", "generate an ipa"
-    option :config, :default => "#{Dir.pwd}/.tug.yml"
-    option :export, :default => "#{Dir.pwd}"
+    option :config, :default => "#{Dir.pwd}/.tug.yml", :aliases => "-c"
+    option :export, :default => "#{Dir.pwd}", :aliases => "-e"
     def ipa
       config_file = Tug::ConfigFile.config_file(options[:config])
       config_file.project.ipa_export_path = options[:export]
@@ -18,7 +18,8 @@ module Tug
     end
 
     desc "provision", "provision system distrubution certificates and provisioning profile"
-    option :config, :default => "#{Dir.pwd}/.tug.yml"
+    option :config, :default => "#{Dir.pwd}/.tug.yml", :aliases => "-c"
+    option :keychain, :default => "login", :aliases => "-k"
     def provision
       config_file = Tug::ConfigFile.config_file(options[:config])
       execute(__method__.to_s, config_file)
